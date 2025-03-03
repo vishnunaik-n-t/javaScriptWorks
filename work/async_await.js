55e385117dde026b103a58d61f1e581e46e28e90
vishnu naik <ntvishnunaik@gmail.com>
2025-03-03 11:17:46 +0530

    learned async await

---
+++ work/async_await.js
@@ -0,0 +1,27 @@
+let fun2= ()=>{
+    console.log("now this is new one");
+}
+
+
+let test=async ()=>{
+    console.log("1");
+    console.log("2");
+  await console.log("3");
+    console.log("4");
+    console.log("hello1");
+}
+
+console.log("first");
+test();
+fun2();
+console.log("5");
+console.log("6");
+console.log("hello2");
+
+
+let fun=()=>{
+    console.log("new func");
+    console.log("new function 2");
+}
+
+fun();
