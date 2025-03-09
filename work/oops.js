018ff25ec1d8ae429ea21816c24db4c89133f274
vishnu naik <ntvishnunaik@gmail.com>
2025-03-09 10:02:16 +0530

    some Ad-JavaScript practiced

--- work/oops.js
+++ work/oops.js
@@ -11,3 +11,102 @@
 
 // obj1.display();
 
+// class bank{
+//     #balence;
+
+//     constructor(balence){
+//         this.#balence=balence;
+//     }
+
+//     deposit(amount){
+//         this.#balence+=amount;
+//     }
+
+//     getBalence(){
+//         return this.#balence;
+//     }
+// }
+
+// let obj=new bank(700);
+// obj.deposit(700);
+// console.log(obj.getBalence());
+ 
+
+// class Animal {
+//     constructor(name) {
+//         if (new.target === Animal) {
+//             // throw new Error("Cannot instantiate an abstract class.");
+//         }
+//         this.name = name;
+//     }
+
+//     makeSound() {
+//         throw new Error("makeSound() must be implemented by subclass");
+//     }
+// }
+
+// class Dog extends Animal {
+
+//     makeSound() {
+//         console.log("Woof! Woof!");
+//     }
+// }
+
+// const dog = new Dog("Buddy");
+// dog.makeSound();  
+
+// const animal = new Animal("Unknown");  
+
+
+// class Animal{
+//     constructor(name){
+//         this.name=name;
+//     }
+
+//     makeSound(){
+//         console.log("woof woof");
+//     }
+// }
+
+// class cat extends Animal{
+//     constructor(model,name){
+//         super();
+//         this.model=model;
+//     }
+
+//     showDetails(){
+//         console.log("model "+this.model);
+//         this.makeSound();
+//     }
+// }
+
+// const Cat=new cat("thor","vishnu");
+// Cat.showDetails();
+// Cat.makeSound();
+
+
+function Animal(name) {
+    this.name = name;
+}
+
+Animal.prototype.makeSound = function () {
+    console.log("Some sound...");
+};
+
+function Dog(name) {
+    Animal.call(this, name);  
+}
+
+Dog.prototype = Object.create(Animal.prototype);  
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.makeSound = function () {
+    console.log("Woof! Woof!");
+};
+
+
+
+const dog = new Dog("Buddy");
+dog.makeSound();
+console.log(dog.name);
+
---
+++ work/practice/demo.js
@@ -0,0 +1,84 @@
+// const mul=(num1, num2)=>{
+//     return num1*num2;
+// }
+
+// console.log(mul(3,5));
+
+
+// const reverse1=(name)=>{
+
+//     let arr=name.split('');
+
+//     for(let i=0,j=name.length-1;i<j;i++,j--){
+//          let temp=arr[i];
+//          arr[i]=arr[j];
+//          arr[j]=temp;
+//     }
+//     return arr.join('');
+    
+// }
+
+// let Name="vishnu";
+// Name=reverse1(Name);
+
+// console.log(Name)
+
+
+// let arr=[1,2,3,4,5,6,7,8,9];
+
+// for (let i=0;i<arr.length;i++){
+//     console.log(arr[i].join(''));
+// }
+// console.log(arr.join(','));
+
+
+// let arr=["vishnu","suchi","panda","sachin","pari"];
+
+// for(let i=0;i<arr.length;i++){
+//     arr[i]=arr[i].split('').reverse().join('');
+// }
+
+// console.log(arr.join(' '));
+
+// let evOrad=(num)=>{
+//     if(num%2==0){
+//         console.log("Even");
+//     }else{
+//         console.log("Add");
+//     }
+// }
+
+// evOrad(98);
+
+// let arr=[true,false,true,false,false,true,true,true,false,true,true];
+// let count=0;
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]) count++;
+// }
+
+// console.log(count);
+
+
+// const check=(num)=>{
+//     let x=3,y=2;
+//     if(num%x==0 && num%y==0){
+//         console.log("yes it is man");
+//     }else{
+//         console.log("nop");
+//     }
+// }
+
+
+// check(6);
+
+// const check=(num)=>{
+//     return num*-1;
+// }
+// console.log(check(10));
+
+// const findsmall=(arr)=>{
+//     return Math.min(...arr);
+// }
+
+// let arr=[33,566,121,34,77,35];
+// console.log(findsmall(arr));
